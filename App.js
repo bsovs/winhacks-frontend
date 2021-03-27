65338b25cde373978f665f1e74144bd7ff41f95b
alexference <alexference23@gmail.com>
2021-03-26 18:33:03 -0700

    App Secondary added

--- App.js
+++ App.js
@@ -3,22 +3,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
+import AppSecondary from './AppSecondary';
 
 export default function App() {
   return (
     <Provider store={configureStore}>
-      <View style={styles.container}>
-        <Text>Testing</Text>
-      </View>
+      <AppSecondary />
     </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
